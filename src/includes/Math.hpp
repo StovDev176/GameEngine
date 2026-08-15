99b9cb3eb0a3a726cc274c3f000ee1b38e5faf6f
StovDev176 <sergedu92000@gmail.com>
2026-08-15 20:25:39 +0200

    Finished and tested the 3D renderer.

--- src/includes/Math.hpp
+++ src/includes/Math.hpp
@@ -1,44 +1,43 @@
 #pragma once
-
 #include <iostream>
 #include <cmath>
 
-struct Vector3 {
+struct vector3 {
   float x, y, z;
 
-  Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
+  vector3(float x, float y, float z) : x(x), y(y), z(z) {}
   
   float Magnitude() const {
     return std::sqrt(x*x + y*y + z*z);
   }
   
-  Vector3 Unit() const {
+  vector3 Unit() const {
     float mag = Magnitude();
-    return Vector3(x/mag, y/mag, z/mag);
+    return vector3(x/mag, y/mag, z/mag);
   }
 
-  Vector3 operator+(const Vector3& other) const {
-    return Vector3(x + other.x, y + other.y, z + other.z);
+  vector3 operator+(const vector3& other) const {
+    return vector3(x + other.x, y + other.y, z + other.z);
   }
   
-  Vector3 operator-(const Vector3& other) const {
-    return Vector3(x - other.x, y - other.y, z - other.z);
+  vector3 operator-(const vector3& other) const {
+    return vector3(x - other.x, y - other.y, z - other.z);
   }
   
-  Vector3 operator*(float scalar) const {
-    return Vector3(x * scalar, y * scalar, z * scalar);
+  vector3 operator*(float scalar) const {
+    return vector3(x * scalar, y * scalar, z * scalar);
   }
 
-  float Dot(const Vector3& other) const {
+  float Dot(const vector3& other) const {
     return ((x * other.x) + (y * other.y) + (z * other.z));
   }
 
-  Vector3 Lerp(const Vector3& other, float t) const {
-    return Vector3(x + (other.x - x) * t, y + (other.y - y) * t, z + (other.z - z) * t);
+  vector3 Lerp(const vector3& other, float t) const {
+    return vector3(x + (other.x - x) * t, y + (other.y - y) * t, z + (other.z - z) * t);
   }
 };
 
-std::ostream& operator<<(std::ostream& os, const Vector3& v) {
+std::ostream& operator<<(std::ostream& os, const vector3& v) {
     os << "(" << v.x << ", " << v.y << ", " << v.z << ")";
     return os;
 }
